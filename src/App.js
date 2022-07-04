be279d765bc53dc5874b504f46c05c489d8c361c
Daniel Kitanaxi Filipe <danielkitanaxifilipe12@gmail.com>
2022-07-04 14:16:39 -0300

    myagenda

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useContext, useEffect} from 'react';
+import { getMonth } from './util'
+import { CalendarHeader } from './components/CalendarHeader';
+import { Menu } from './components/Menu';
+import { Month } from './components/Month';
+import { Sidebar } from './components/Sidebar';
+import GlobalContext from './context/GlobalContext';
+import { EventModal } from './components/EventModal';
+function AppTest() {
+const [currenMonth, setCurrenMonth] = useState (getMonth());
+const {monthIndex, showEventModal} = useContext(GlobalContext);
 
-function App() {
+  useEffect(() => {
+    setCurrenMonth(getMonth(monthIndex));
+  }, [monthIndex]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Menu/>    
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-3">
+            <Sidebar/>
+            <div className="card card-alert">
+                <div className="card-body">
+                  <h5>Upgrade to Primium</h5>
+                  <font className="font-13px">Go Pro unlock all features</font> <br />
+                  <button type="button" className="btn btn-light font-14px mt-3">Upgrade Now!</button>
+                </div>
+              </div>
+          </div>
+          <div className="col-md-9">
+          {showEventModal && <EventModal/>}      
+          <CalendarHeader/>
+          <Month month={currenMonth} />
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
 
-export default App;
+export default AppTest;
